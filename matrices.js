f41161d754017014c5f1c2698533da29421fbe02
leocantu17 <140271635+leocantu17@users.noreply.github.com>
2023-07-27 12:26:34 -0600

    Arreglo

---
+++ matrices.js
@@ -0,0 +1,15 @@
+let arregloMatriz1=[[]];
+let arregloMatriz2=[[]]
+
+
+const botonResultado=document.querySelector('#resultado');
+
+botonResultado.addEventListener('click',saludar);
+
+function saludar(){
+    console.log("Hola");
+}
+
+
+
+
